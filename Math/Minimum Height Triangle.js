2983d24bdc830aaba399754942c71866e29691e1
Ahmed Abdallah <Ahmedabdailla@gmail.com>
2020-04-22 21:23:36 +0200

    Min Height Triangle

---
+++ Math/Minimum Height Triangle.js
@@ -0,0 +1,39 @@
+//Link = https://www.hackerrank.com/challenges/lowest-triangle/problem
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function lowestTriangle(base, area){
+    
+    let height = (2*area)/base;
+
+    height = Math.ceil(height);
+    return height;
+}
+
+function main() {
+    var base_temp = readLine().split(' ');
+    var base = parseInt(base_temp[0]);
+    var area = parseInt(base_temp[1]);
+    var height = lowestTriangle(base, area);
+    process.stdout.write(""+height+"\n");
+
+}
